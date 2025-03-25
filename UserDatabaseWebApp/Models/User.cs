cfd0f841898d024a1dd6c50001a6f266c21dd7ad
Shashag Gashash <por_favor04983@outlook.com>
2025-03-26 01:36:53 +0300

    Add project files.

---
+++ UserDatabaseWebApp/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UserDatabaseWebApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        public string Password { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime LastSeen { get; set; }
+        public bool Blocked { get; set; }
+    }
+}
---
+++ Users/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Users.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Users.Data.UsersContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Users.Data.UsersContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
