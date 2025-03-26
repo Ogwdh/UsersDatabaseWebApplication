c162db1c632a14739aea7b299bfa670ec1c63c4a
Shashag Gashash <por_favor04983@outlook.com>
2025-03-26 15:33:41 +0300

    Remote database connection

---
+++ UserDatabaseWebApp/Data/UserDatabaseWebAppContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace UserDatabaseWebApp.Data
+{
+    public class UserDatabaseWebAppContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public UserDatabaseWebAppContext() : base("name=UserDatabaseWebAppContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<UserDatabaseWebApp.Models.User> Users { get; set; }
+    }
+}
---
+++ UserDatabaseWebApp/Migrations/202503261034391_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿namespace UserDatabaseWebApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Email = c.String(),
+                        Password = c.String(),
+                        LastSeen = c.DateTime(nullable: false),
+                        Blocked = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
